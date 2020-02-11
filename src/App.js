de4a571b0817e2197bf23649b27e59ce8d5efde8
alex.paclin <alex.paclin@gmail.com>
2020-02-11 18:12:35 +0500

    test environment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
+import Todo from './Todo'
 
 export default class App extends Component {
-	render () {
-		return (
-			<div></div>
-		)
-	}
+  render() {
+    return <Todo/>
+  }
 }
